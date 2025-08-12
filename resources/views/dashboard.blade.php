fc06510ce15b82b6ded5668d6ddffc63fd32b4e5
Mariana de Souza Gomes <mariana.gomes@adagri.local>
2025-08-12 16:33:03 -0300

    editando ntml

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,14 +12,14 @@
                 <div class="mt-6">
                     <a href="{{ route('colaboradores.create') }}"
                        class="inline-block bg-gray-500 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">
-                        ➕ Adicionar Colaborador
+                         Adicionar Colaborador
                     </a>
                 </div>
 
                 <div class="mt-3">
                     <a href="{{ route('colaboradores.index') }}"
                        class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        👀 Visualizar Colaboradores
+                         Visualizar Colaboradores
                     </a>
                 </div>
             </div>
